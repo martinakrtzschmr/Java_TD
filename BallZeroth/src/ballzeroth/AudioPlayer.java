9e709c94e12bfe27c28ea9ff944d781e52ba509e
sirloris <47677782+sirloris@users.noreply.github.com>
2019-05-14 17:04:10 -0300

    class

---
+++ BallZeroth/src/ballzeroth/AudioPlayer.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ballzeroth;
+
+import org.newdawn.slick.Sound;
+
+/**
+ *
+ * @author victor.lopes
+ */
+public class AudioPlayer {
+}
