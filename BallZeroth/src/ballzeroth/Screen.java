9505eea5e974d364593890707924f8d6344ddaa0
Martin Alexander Kretzschmar <alexander.martin64@gmail.com>
2019-05-14 19:37:59 -0300

    some changes

--- BallZeroth/src/ballzeroth/Screen.java
+++ BallZeroth/src/ballzeroth/Screen.java
@@ -4,8 +4,6 @@ import java.awt.*; // Importa todas as classes AWT para o projeto
 import java.awt.image.*; // Importa todas as classes Image para o projeto
 import javax.swing.*; // Importa todas as classes SWING para o projeto
 import java.io.*;
-import java.nio.file.Files;
-import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,7 +21,6 @@ public class Screen extends JPanel implements Runnable {
     private ImageIcon image;
     
     public Screen() {
-        setBackground(Color.BLUE);
         running = true;
         gameLoop.start();
     }
@@ -32,29 +29,22 @@ public class Screen extends JPanel implements Runnable {
     public void paintComponent (Graphics g) {
         if(start) {
             try {
+                screenWidth = getWidth(); // function comes from JPanel extension
+                screenHeight = getHeight(); // function comes from JPanel extension
+
+                map = new Map();
                 start();
                 start = false;
             } catch (IOException ex) {
                 Logger.getLogger(Screen.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+        
         g.clearRect(0, 0, getWidth(), getHeight());
         map.draw(g); // Draw the map and update it
     }
     
     public void start () throws FileNotFoundException, IOException {
-        screenWidth = getWidth(); // function comes from JPanel extension
-        screenHeight = getHeight(); // function comes from JPanel extension
-        
-        map = new Map();
-        
-        for (int i = 0; i < tileset_ground.length; i++) {
-            image = new ImageIcon(SpriteIDs.imagesDIR);
-            //image = new ImageIcon(getClass().getClassLoader().getResource(SpriteIDs.imagesDIR));
-            tileset_ground[i] = image.getImage();
-            tileset_ground[i] = createImage( new FilteredImageSource(tileset_ground[i].getSource(), new CropImageFilter(0, 64 * i, 64, 64)) );
-        }
-        
         try {
             String linha="";
             int counter = 1;
@@ -66,14 +56,26 @@ public class Screen extends JPanel implements Runnable {
             while((linha = sc.readLine()) != null){
                 String caracteres[] = linha.split(" ");
                 
+                System.out.print("Caracter: ");
+                for (int i = 0; i < caracteres.length; i++) {
+                    System.out.print(caracteres[i] + " ");
+                }
+                System.out.println("");
+                
                 for (int y = 0; y < counter; y++) {
                     for (int x = 0; x < caracteres.length; x++) {
                         map.block[y][x].terrainID = Integer.parseInt(caracteres[x]);
-                        System.out.println("terrain: " + map.block[y][x].terrainID);
                     }
                 }
                 
                 counter++;
+            }
+            
+            for (int i = 0; i < tileset_ground.length; i++) {
+                image = new ImageIcon(SpriteIDs.imagesDIR);
+                //image = new ImageIcon(getClass().getClassLoader().getResource(SpriteIDs.imagesDIR));
+                tileset_ground[i] = image.getImage();
+                tileset_ground[i] = createImage( new FilteredImageSource(tileset_ground[i].getSource(), new CropImageFilter(0, 64 * i, 64, 64)) );
             }
 
             sc.close();
@@ -83,18 +85,14 @@ public class Screen extends JPanel implements Runnable {
     }
     
     public void run() {
-        long lastTime = System.nanoTime();
-        double amountOfTicks = 60.0;
-        double ns = 1000000000 / amountOfTicks;
-        double delta = 0;
-        long timer = System.currentTimeMillis();
-        int frames = 0;
-    
+//        long lastTime = System.nanoTime();
+//        double amountOfTicks = 60.0;
+//        double ns = 1000000000 / amountOfTicks;
+//        double delta = 0;
+//        long timer = System.currentTimeMillis();
+//        int frames = 0;
+//    
         while(running) {
-            if(!start){
-                
-            }
-            
             repaint();
             
             try {
