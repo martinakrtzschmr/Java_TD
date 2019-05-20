244e5087e8dc77cfb096391b0b19f8eb8c85c0d0
sirloris <47677782+sirloris@users.noreply.github.com>
2019-05-20 14:39:44 -0300

    sound class
    implementado a classe sounds, porém precisa verificar como chamar dentro das variaveis

--- BallZeroth/src/ballzeroth/BallZeroth.java
+++ BallZeroth/src/ballzeroth/BallZeroth.java
@@ -1,7 +1,9 @@
 package ballzeroth;
 
-import javax.swing.JFrame;
 import java.awt.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.JFrame;
 
 public class BallZeroth extends JFrame {
     public static String title = "BallZeroth!";
@@ -11,22 +13,32 @@ public class BallZeroth extends JFrame {
         this.setLayout(new GridLayout(1,1,0,0));
         Screen screen = new Screen(this); // Objeto que extende a classe JPanel
         this.add(screen);
-        
+       
         this.setVisible(true);
     }
     
+    public static ArrayList <Sounds> loadSounds() throws MalformedURLException{
+        ArrayList <Sounds> sounds = new ArrayList();
+        sounds.add(new Sounds(".\\src\\resources\\Main Title _ Legends of Azeroth - World of Warcraft [music].wav"));
+        
+        
+        return sounds; 
+    }
+    
     // Utilize "this." para verificar as funções extendidas pelo JFrame
-    public BallZeroth () {
+    public BallZeroth () throws MalformedURLException {
         this.setTitle(title);
         this.setSize(size);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
+        ArrayList <Sounds> sounds = loadSounds();   
+        
         init();
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
         BallZeroth game = new BallZeroth();
     }
 }
